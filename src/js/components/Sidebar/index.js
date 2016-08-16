ad6891e3feccbe046f055164055471bbaa55bf11
maiiivuu <vu.mai@visma.com>
2016-08-16 10:42:49 +0200

    KLAS-378: Set static contactPerson on frontpage to Anne Gro

--- src/js/components/Sidebar/index.js
+++ src/js/components/Sidebar/index.js
@@ -3,11 +3,47 @@ import React, { Component, PropTypes } from 'react'
 import counterpart from 'counterpart'
 import Translate from 'react-translate-component'
 import LocaleSwitcher from './LocaleSwitcher'
+import _ from 'lodash'
 
 class Sidebar extends Component{
 
+    renderContactInfo () {
+        const { contactInfo } = this.props
+
+        if (_.isObject(contactInfo)) {
+            return (
+                <div className="contact">
+                    <p className="name">{contactInfo.name}</p>
+                    <p className="e-mail">
+                        <span className="key"><Translate content="CONTACT.EMAIL" />: </span>
+                        <a className="value" href={"mailto:" + contactInfo.email}>{contactInfo.email}</a>
+                    </p>
+                    <p className="phone">
+                        <span className="key"><Translate content="CONTACT.PHONE" />: </span>
+                        <span className="value">{contactInfo.phone}</span>
+                    </p>
+                </div>
+            )
+        } else {
+            return (
+                <div className="contact">
+                    <p className="name">{contactInfo}</p>
+                    <p className="e-mail">
+                        <span className="key"><Translate content="CONTACT.EMAIL" />: </span>
+                        <a className="value" href="mailto:anne.gro.hustoft@ssb.no">anne.gro.hustoft@ssb.no</a>
+                    </p>
+                    <p className="phone">
+                        <span className="key"><Translate content="CONTACT.PHONE" />: </span>
+                        <span className="value">62 88 55 48</span>
+                    </p>
+                </div>
+            )
+        }
+
+    }
+
 	render () {
-		const { contactInfo, onLanguageChange, actions, params } = this.props
+		const { onLanguageChange, actions, params } = this.props
 		return (
 			<div className="sidebar">
 				<LocaleSwitcher onLanguageChange={onLanguageChange} actions={actions} params={params} />
@@ -16,17 +52,7 @@ class Sidebar extends Component{
                     <h2><span className="overskrift-tekst">
                         <Translate content="CONTACT.HEADER" />
                     </span></h2>
-                    <div className="contact">
-                        <p className="name">{contactInfo.name}</p>
-                        <p className="e-mail">
-                            <span className="key"><Translate content="CONTACT.EMAIL" />: </span>
-                            <a className="value" href={"mailto:" + contactInfo.email}>{contactInfo.email}</a>
-                        </p>
-                        <p className="phone">
-                            <span className="key"><Translate content="CONTACT.PHONE" />: </span>
-                            <span className="value">{contactInfo.phone}</span>
-                        </p>
-                    </div>
+                    {this.renderContactInfo()}
 				</div>
 			</div>
 		)
@@ -34,7 +60,7 @@ class Sidebar extends Component{
 }
 
 Sidebar.propTypes = {
-	contactInfo: PropTypes.object.isRequired, // Should be object?
+	// contactInfo: PropTypes.object.isRequired, // Should be object?
 	onLanguageChange: PropTypes.func,
 	params: PropTypes.object,
 	actions: PropTypes.object
