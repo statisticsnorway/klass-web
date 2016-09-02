38ae9cc9d9fdc04e52e1d069c782f3614976784c
maiiivuu <vu.mai@visma.com>
2016-09-02 14:44:06 +0200

    Added localization

--- src/js/locales/en.js
+++ src/js/locales/en.js
@@ -73,7 +73,7 @@ module.exports = {
 		'OWNING_SECTION': 'Responsible division',
 		'INCLUDE_CODELISTS': 'Include codelists',
 		'WHAT_IS_CODELIST_HEADER': 'What is a codelist?',
-		'WHAT_IS_CODELIST_BODY': 'Lorem Ipsum er rett og slett dummytekst fra og for trykkeindustrien. Lorem Ipsum har vært bransjens standard for dummytekst helt siden 1500-tallet, da en ukjent boktrykker stokket en mengde bokstaver for å lage et prøveeksemplar av en bok. Lorem Ipsum har tålt tidens tann usedvanlig godt, og har i tillegg til å bestå gjennom fem århundrer også tålt spranget over til elektronisk typografi uten vesentlige endringer. Lorem Ipsum ble gjort allment kjent i 1960-årene ved lanseringen av Letraset-ark med avsnitt fra Lorem Ipsum, og senere med sideombrekkingsprogrammet Aldus PageMaker som tok i bruk nettopp Lorem Ipsum for dummytekst.',
+		'WHAT_IS_CODELIST_BODY': 'Code lists are usually adapted to specific needs (surveys, statistics), and cover only the categories used within that particular area. The categories in a classification are exhaustive and mutually exclusive, this is not the case for code lists.',
 		'BACK_TO_CLASSIFICATION_OVERVIEW': 'Back to classifications and codelists overview',
         'SEARCH_ERROR': '* Searchstring required'
 	},
