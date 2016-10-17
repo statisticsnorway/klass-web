aeb80446ef8c7a4aa83170a0b4b5c2988499d43d
mlo <mlo@ssb.no>
2016-10-17 14:50:56 +0200

    Replacing "demo 404" text page with norwegian text, might need to replace with translatable text later

--- src/js/views/NotFoundView/index.js
+++ src/js/views/NotFoundView/index.js
@@ -5,9 +5,9 @@ export default class NotFoundView extends Component {
   render () {
     return (
       <div className='container text-center'>
-        <h1>This is a demo 404 page!</h1>
-        <hr />
-        <Link to='/'>Back To Home View</Link>
+        <h1>Siden ble ikke funnet - 404</h1>
+          <br/>
+        <p>Du har dessverre havnet på en side som ikke finnes.</p>
       </div>
     );
   }
