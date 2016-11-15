92c044097f9c00eb9f20c446dc68dde135c280db
mlo <mlo@ssb.no>
2016-11-15 14:59:52 +0100

    Added QA environment  profile

---
+++ src/env/config/qa/index.js
@@ -0,0 +1,17 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+// WARNING!
+// /config/index.js will be replaced with files from env/config/ when maven builds are executed.
+// changes should be done in the  /env/config/ files
+////////////////////////////////////////////////////////////////////////////////////////////////
+const config = {
+	API_BASE_URL: 'http://beta-data.ssb.no/api/klass/v1/',
+	API_LOCAL_BASE_URL: 'http://localhost:3001',
+	API_LOCAL_STORAGE: false,
+	LANGUAGES: {
+		BOKMAL: 'nb',
+		NYNORSK: 'nn',
+		ENGLISH: 'en'
+	}
+}
+
+export default config;
