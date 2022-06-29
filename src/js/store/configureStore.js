758e8eba51aacbc65576efb153ea8219cf9db5c5
Rune Johansen <runejo@gmail.com>
2022-06-29 09:46:52 +0200

    Upgrade misc dependencies

--- src/js/store/configureStore.js
+++ src/js/store/configureStore.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import api from '../middleware/api';
 import rootReducer from '../reducers';
-import createLogger from 'redux-logger';
+import { createLogger } from 'redux-logger';
 import { persistState } from 'redux-devtools';
 
 export default function configureStore(initialState) {
