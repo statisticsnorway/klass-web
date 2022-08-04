41454ed0996f79a624d53377172e9010d3bf181e
Rune Johansen <runejo@gmail.com>
2022-08-04 16:53:17 +0200

    Use webpack-merge to have common webpack config

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,83 +1,10 @@
-const path = require('path');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.config.common.js');
 
-// App files location
-const PATHS = {
-  app: path.resolve(__dirname, '../src/js'),
-  styles: path.resolve(__dirname, '../src/styles'),
-  images: path.resolve(__dirname, '../src/images'),
-  static: path.resolve(__dirname, '../src/static'),
-  build: path.resolve(__dirname, '../build')
-};
-
-const plugins = [
-  new CopyWebpackPlugin({
-    patterns: [
-      { from: PATHS.images, to: "images" },
-      { from: PATHS.static, to: "static" },
-    ],
-  }),
-
-  // This plugin moves all the CSS into a separate stylesheet
-  new MiniCssExtractPlugin({
-    filename: "css/[name].css"
-  }),
-
-    // Needed by counterpart
-    new NodePolyfillPlugin()
-
-];
-
-module.exports = {
-  entry: {
-    app: path.resolve(PATHS.app, 'main.js'),
-    vendor: ['react']
-  },
-  mode: "production",
+module.exports = merge(common, {
+  mode: 'production',
   output: {
-    path: PATHS.build,
-    filename: 'js/[name].js',
     publicPath: '/klass-ssb-no/'
   },
-  stats: {
-    colors: true,
-    reasons: true,
-    orphanModules: false,
-  },
-  resolve: {
-    // We can now require('file') instead of require('file.jsx')
-    extensions: ['.js', '.jsx', '.scss'],
-  },
-  module: {
-    noParse: /\.min\.js$/,
-    rules: [
-      {
-        test: /\.jsx?$/,
-        use: ['babel-loader'],
-        include: PATHS.app
-      },
-      {
-        test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
-      },
-      // Inline base64 URLs for <=8k images, direct URLs for the rest
-      {
-        test: /\.(png|jpg|jpeg|gif|svg)$/,
-        type: 'asset'
-      },
-      {
-        test: /\.(woff|woff2)$/,
-        type: 'asset'
-      }
-    ]
-  },
-  plugins: plugins,
-  optimization: {},
-  devtool: 'source-map'
-};
+  devtool: 'source-map',
+});
