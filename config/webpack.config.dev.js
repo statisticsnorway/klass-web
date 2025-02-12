1ae8d87d87aab64d465c528cf2d0a995de8c39ec
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-02-12 18:40:04 +0100

    remove headers

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -16,20 +16,14 @@ module.exports = merge(common, {
     open: true,
     port: 3000,
     allowedHosts: ['klass-web.intern.test.ssb.no', 'klass-web.intern.ssb.no'],
-    headers: {
-      "Access-Control-Allow-Origin": "*",  // Allow all origins, change this to a specific domain in production
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-      "Access-Control-Allow-Headers": "Content-Type, Authorization",
-      "Access-Control-Allow-Credentials": "true"
-    },
     proxy: [
       {
-      '/klass': {
+      '/': {
         target: 'http://localhost:3001', // Your backend API URL
         changeOrigin: true,  // This allows the proxy to work across different origins
         secure: false,       // Set to false if your backend doesn't use HTTPS
         pathRewrite: {
-          '^/klass': '', // Optional: rewrite the URL before forwarding it to the backend
+          '^/': '', // Optional: rewrite the URL before forwarding it to the backend
         },
       },
     },
