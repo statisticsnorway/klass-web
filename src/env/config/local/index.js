16a5e533dc3556ac7e47b0576db430ae4c4e83c2
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2025-02-14 16:45:24 +0100

    Use https for all requests

--- src/env/config/local/index.js
+++ src/env/config/local/index.js
@@ -4,16 +4,16 @@
 // changes should be done in the  /env/config/ files
 ////////////////////////////////////////////////////////////////////////////////////////////////
 const config = {
-	API_BASE_URL: '//data.ssb.no/api/klass/v1',
-	API_LOCAL_BASE_URL: 'http://localhost:3001',
-	API_LOCAL_STORAGE: false,
-	OM_KLASS_URL: 'https://www.ssb.no/metadata/om-klass',
-	OM_KLASS_URL_EN: 'https://www.ssb.no/en/metadata/om-klass',
-	LANGUAGES: {
-		BOKMAL: 'nb',
-		NYNORSK: 'nn',
-		ENGLISH: 'en'
-	},
-}
+  API_BASE_URL: "https://data.ssb.no/api/klass/v1",
+  API_LOCAL_BASE_URL: "http://localhost:3001",
+  API_LOCAL_STORAGE: false,
+  OM_KLASS_URL: "https://www.ssb.no/metadata/om-klass",
+  OM_KLASS_URL_EN: "https://www.ssb.no/en/metadata/om-klass",
+  LANGUAGES: {
+    BOKMAL: "nb",
+    NYNORSK: "nn",
+    ENGLISH: "en",
+  },
+};
 
 export default config;
--- src/js/components/Sidebar/ApiLinks.js
+++ src/js/components/Sidebar/ApiLinks.js
@@ -1,24 +1,29 @@
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
-import Translate from 'react-translate-component'
+import PropTypes from "prop-types";
+import React, { Component } from "react";
+import Translate from "react-translate-component";
 
 class ApiLinks extends Component {
-
-	render() {
-        return (
-            <div id="apiLinks">
-                <h2><Translate content="API.API_HEADER"/></h2>
-                <div className="side-content-wrapper">
-                    <a className="apilink" href="//data.ssb.no/api/klass/v1/api-guide.html"><Translate content="API.API_LINK"/></a>
-                </div>
-            </div>
-
-        )
-	}
+  render() {
+    return (
+      <div id="apiLinks">
+        <h2>
+          <Translate content="API.API_HEADER" />
+        </h2>
+        <div className="side-content-wrapper">
+          <a
+            className="apilink"
+            href="https://data.ssb.no/api/klass/v1/api-guide.html"
+          >
+            <Translate content="API.API_LINK" />
+          </a>
+        </div>
+      </div>
+    );
+  }
 }
 
-ApiLinks.propTypes = {}
+ApiLinks.propTypes = {};
 
-ApiLinks.defaultProps = {}
+ApiLinks.defaultProps = {};
 
-export default ApiLinks
+export default ApiLinks;
