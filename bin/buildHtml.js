4b16bdfa56e0eadd497df7f2e5ef65d077d75a91
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2025-02-18 11:58:30 +0100

    Remove unneeded files and dependencies (#514)
    * Remove colors dependency
    
    * Add npm run docker
    
    * Remove unused dependencies
    
    * Update README
    
    * Regenerate lock file, Dockefile improvements

--- bin/buildHtml.js
+++ bin/buildHtml.js
@@ -6,11 +6,10 @@
 
 /*eslint-disable no-console */
 
-var fs = require('fs');
-var colors = require('colors');
-var cheerio = require('cheerio');
+var fs = require("fs");
+var cheerio = require("cheerio");
 
-fs.readFile('src/index.html', 'utf8', (err, markup) => {
+fs.readFile("src/index.html", "utf8", (err, markup) => {
   if (err) {
     return console.log(err);
   }
@@ -18,13 +17,13 @@ fs.readFile('src/index.html', 'utf8', (err, markup) => {
   const $ = cheerio.load(markup);
 
   // Since a separate spreadsheet is only utilized for the production build, need to dynamically add this here.
-  $('head').append('<link rel="stylesheet" href="./css/app.css">');
+  $("head").append('<link rel="stylesheet" href="./css/app.css">');
 
-  fs.writeFile('build/index.html', $.html(), 'utf8', function (err) {
+  fs.writeFile("build/index.html", $.html(), "utf8", function (err) {
     if (err) {
       return console.log(err);
     }
   });
 
-  console.log('index.html written to /build'.green);
+  console.log("index.html written to /build".green);
 });
