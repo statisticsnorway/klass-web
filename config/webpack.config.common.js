2b0053f2a682aaaa64b9b0593cd8c00a924577d9
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2025-02-19 08:42:48 +0100

    Fix routing to static files (#519)
    * Fix rewrite for static files
    
    * Simplify build
    
    * Remove even more cruft

--- config/webpack.config.common.js
+++ config/webpack.config.common.js
@@ -1,17 +1,18 @@
-const path = require('path');
+const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
-webpack = require('webpack')
+webpack = require("webpack");
 
 module.exports = {
   entry: {
-    app: path.resolve(path.resolve(__dirname, '../src/js'), 'main.js'),
+    app: path.resolve(path.resolve(__dirname, "../src/js"), "main.js"),
   },
   output: {
-    path: path.resolve(__dirname, '../build'),
-    filename: 'js/[name].js',
+    path: path.resolve(__dirname, "../build"),
+    filename: "js/[name].js",
   },
   stats: {
     colors: true,
@@ -20,71 +21,80 @@ module.exports = {
   },
   resolve: {
     // We can now require('file') instead of require('file.jsx')
-    extensions: ['.js', '.jsx', '.scss'],
+    extensions: [".js", ".jsx", ".scss"],
     fallback: {
-      "process": require.resolve("process/browser"),
-      "path": require.resolve("path-browserify"),
-      "stream": require.resolve("stream-browserify"),
-      "util": require.resolve("util/"),
+      process: require.resolve("process/browser"),
+      path: require.resolve("path-browserify"),
+      stream: require.resolve("stream-browserify"),
+      util: require.resolve("util/"),
     },
   },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
-        use: ['babel-loader'],
-        include: path.resolve(__dirname, '../src/js')
+        use: ["babel-loader"],
+        include: path.resolve(__dirname, "../src/js"),
       },
       {
         test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
       },
       // Inline base64 URLs for <=8k images, direct URLs for the rest
       {
         test: /\.(png|jpg|jpeg|gif|svg)$/,
-        type: 'asset'
+        type: "asset",
       },
       {
         test: /\.(woff|woff2)$/,
-        type: 'asset'
-      }
-    ]
+        type: "asset",
+      },
+    ],
   },
   plugins: [
     new CopyWebpackPlugin({
       patterns: [
-        { from: path.resolve(__dirname, '../src/images'), to: "images" },
-        { from: path.resolve(__dirname, '../src/static'), to: "static" },
+        { from: path.resolve(__dirname, "../src/images"), to: "images" },
+        { from: path.resolve(__dirname, "../src/static"), to: "static" },
+        {
+          from: path.resolve(__dirname, "../src/index.html"),
+          to: "index.html",
+        },
       ],
     }),
-    new webpack.ProvidePlugin({ process: "process/browser", Buffer: ["buffer", "Buffer"], 
-
+    new webpack.ProvidePlugin({
+      process: "process/browser",
+      Buffer: ["buffer", "Buffer"],
     }),
 
     // This plugin moves all the CSS into a separate stylesheet
     new MiniCssExtractPlugin({
-      filename: "css/[name].css"
+      filename: "static/css/[name].css",
     }),
 
     // https://github.com/webpack-contrib/webpack-bundle-analyzer/blob/master/README.md
-    new BundleAnalyzerPlugin({ analyzerMode: 'disabled' }) // default is 'server'
-
+    new BundleAnalyzerPlugin({ analyzerMode: "disabled" }), // default is 'server'
   ],
   optimization: {
-    moduleIds: 'deterministic',
-    runtimeChunk: 'single',
+    moduleIds: "deterministic",
+    runtimeChunk: "single",
     splitChunks: {
       cacheGroups: {
         vendor: {
           test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all'
-        }
-      }
-    }
+          name: "vendors",
+          chunks: "all",
+        },
+      },
+    },
   },
 };
