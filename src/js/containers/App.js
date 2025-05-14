54e9c4759c36dac318fd63fb4b8f5041ed921971
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-05-14 08:21:46 +0200

    remove changes beradcrumbs

--- src/js/containers/App.js
+++ src/js/containers/App.js
@@ -6,34 +6,21 @@ import Translate from 'react-translate-component'
 class App extends Component {
 	renderBreadcrumbs () {
 
-        const classification = this.state
-        const { routes, params } = this.props
         return (
-            <div id="breadcrumbs">
-                <nav className="row path">
-                    <h2 className="screen-reader-only"><Translate content="COMMON.YOU_ARE_HERE" />:</h2>
-                    <div className="ssb-breadcrumbs">
-                        <a href="https://www.ssb.no/" className="ssb-link">
-                            <span className="link-text">Forsiden </span>
-                        </a>
-                    </div>
-                    <div className="ssb-breadcrumbs">
-                        <a href="https://www.ssb.no/metadata/" className="ssb-link">
-                            <span className="link-text">Metadata</span>
-                        </a>
-                    </div>
-                    <div className="step ssb-breadcrumbs">
-                        <Breadcrumbs
-                            routes={this.props.routes}
-                            params={this.props.params}
-                            itemClass="step"
-                            excludes={['exclude']}
-                        />
-                    </div>
-                </nav>
+            <div id="navigation-path">
+                <h2 className="screen-reader-only"><Translate content="COMMON.YOU_ARE_HERE" />:</h2>
+                <span><a href="//www.ssb.no/"><Translate content="COMMON.FRONTPAGE" /></a>{' > '}</span>
+                <span><a href="//www.ssb.no/metadata/">Metadata</a>{' > '}</span>
+                <Breadcrumbs
+                    routes={this.props.routes}
+                    params={this.props.params}
+                    itemClass="step"
+                    excludes={['exclude']}
+                    />
             </div>
         )
     }
+
 
     render () {
         return (
