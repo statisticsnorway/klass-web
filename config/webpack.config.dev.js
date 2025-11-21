4492a9f0e6939ad9fa3a32b222181ff596e6b592
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-11-21 10:54:49 +0100

    redo

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -1,6 +1,6 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.config.common.js");
-const path = require("path");
+const path = require("node:path");
 
 module.exports = merge(common, {
 	mode: "development",
